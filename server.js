878af6aabe502a3f7264bc9b9fc09abb78e322e9
Nabanita Bag <ashley.makruz@gmail.com>
2021-02-17 20:37:53 +0530

    set up server on port 3000

---
+++ server.js
@@ -0,0 +1,9 @@
+console.log("my first express server is here!");
+
+const express = require("express");
+const app = express();
+
+app.listen(3000, function()
+{
+  console.log("Server started on port 3000.");
+});
