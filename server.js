b54ab1cd84ed4b0e91952de4a06a57c2518a6a9b
Nabanita Bag <ashley.makruz@gmail.com>
2021-02-17 21:30:02 +0530

    send response over server

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ console.log("my first express server is here!");
 const express = require("express");
 const app = express();
 
+app.get("/", function(request, response)
+{
+  //console.log(request);
+  response.send("<h1>hi</h1>");
+})
 app.listen(3000, function()
 {
   console.log("Server started on port 3000.");
